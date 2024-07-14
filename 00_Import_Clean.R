06e085492090b23c3c044e7cbf933cd0f42650c7
Jean-Rémi Telmon <jeanremi.telmon@free.fr>
2024-07-14 18:53:26 +0100

    adding weather for daily and join

--- 00_Import_Clean.R
+++ 00_Import_Clean.R
@@ -10,7 +10,9 @@ aep_daily <- aep_hourly |>
     datetime_col = datetime,
     value_col = aep_mw, 
     fn_reduction = max, 
-    time_unit = "month")
+    time_unit = "day",
+    output_col = "daily_peak_MW"
+  )
 
 # Weather
 weather_colombus <- weather_history(
@@ -27,4 +29,28 @@ weather_colombus <- weather_history(
   )
 )
 
+weather_colombus_day <- weather_colombus |> 
+mutate(date = lubridate::floor_date(datetime, "day")) |> 
+summarise(
+  .by = date,
+  across(
+    contains(c("temperature", "wind_speed_10m")),
+    list(mean = mean, min = min, max = max),
+    .names = "{.col}.{.fn}"
+  ),
+  across(
+    contains(c("relative_humidity_2m", "cloud_cover")),
+    list(mean = mean),
+    .names = "{.col}.{.fn}"
+  ),
+  across(
+    contains(c("precipitation")),
+    list(mean = mean),
+    .names = "{.col}.{.fn}"
+  )
+)
+
+XY <- left_join(aep_daily, weather_colombus_day, by = join_by(date==date))
 write_rds(weather_colombus, "data/processed/weather_colombus.rds")
+write_rds(weather_colombus_day, "data/processed/weather_colombus_day.rds")
+write_rds(XY, "data/processed/XY.rds")
